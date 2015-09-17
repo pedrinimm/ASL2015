5288a49fb22cf955af2cbcc7b751e74ce3f13e5f
pedrinimm <pememo8@gmail.com>
2015-09-17 15:10:21 +0200

    The library for the database connection is added, the build ant is blank, the class in charge of the database connection is not tested yet

---
+++ src/server/DatabaseConnectorServer.java
@@ -0,0 +1,58 @@
+package server;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.postgresql.ds.PGPoolingDataSource;
+
+public class DatabaseConnectorServer {
+	
+	//pool of connections for the database
+	private final PGPoolingDataSource connectionPool;
+	
+	//constructor of the class
+	public DatabaseConnectorServer(){
+		
+		connectionPool= new PGPoolingDataSource();
+		
+	}
+	
+	//method for setting up the connection to the database
+	public void setupDatabaseConnectionPool(String username,
+            String password, String server, String database, int maxConnections) {
+        		connectionPool.setUser(username);
+        		connectionPool.setPassword(password);
+        		connectionPool.setServerName(server);
+        		connectionPool.setDatabaseName(database);
+        		connectionPool.setMaxConnections(maxConnections);
+    }
+	
+	//method for requesting a new connection to the database
+	public Connection getDatabaseConnection(){
+		Connection con;
+		try {
+			con = connectionPool.getConnection();
+			//with auto-commit on false no instruction will be applied until I set commit
+			//manually
+			con.setAutoCommit(false);
+	        return con;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	//method to close the connection with the database
+	public void closeConnection(){
+		connectionPool.close();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
